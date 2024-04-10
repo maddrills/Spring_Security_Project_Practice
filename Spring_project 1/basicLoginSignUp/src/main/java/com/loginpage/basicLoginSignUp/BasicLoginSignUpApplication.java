f12d6f1de5d8cbeb80e12287418bc72757b3db85
maddrills <mathewfvv@gmail.com>
2024-04-10 19:08:31 +0530

    First (BLS) Basic Login SignUp project desc

---
+++ Spring_project 1/basicLoginSignUp/src/main/java/com/loginpage/basicLoginSignUp/BasicLoginSignUpApplication.java
@@ -0,0 +1,13 @@
+package com.loginpage.basicLoginSignUp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicLoginSignUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicLoginSignUpApplication.class, args);
+	}
+
+}
