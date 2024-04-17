17f2ae2e56de6de7ba71b0f9a0b5513869861743
maddrills <mathewfvv@gmail.com>
2024-04-17 16:36:25 +0530

    JWT Front End Interceptor

--- SpringSecurityPraactice/Eight Token based Authentication using JSON Web Token (JWT)/bank-app-ui/src/app/components/login/login.component.ts
+++ SpringSecurityPraactice/Eight Token based Authentication using JSON Web Token (JWT)/bank-app-ui/src/app/components/login/login.component.ts
@@ -1,40 +1,41 @@
 import { Component, OnInit } from '@angular/core';
-import { User } from "src/app/model/user.model";
+import { User } from 'src/app/model/user.model';
 import { NgForm } from '@angular/forms';
 import { LoginService } from 'src/app/services/login/login.service';
 import { Router } from '@angular/router';
 import { getCookie } from 'typescript-cookie';
 
-
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-  authStatus: string = "";
+  authStatus: string = '';
   model = new User();
 
-  constructor(private loginService: LoginService, private router: Router) {
+  constructor(private loginService: LoginService, private router: Router) {}
 
-   }
-
-  ngOnInit(): void {
-
-  }
+  ngOnInit(): void {}
 
   validateUser(loginForm: NgForm) {
-    this.loginService.validateLoginDetails(this.model).subscribe(
-      responseData => {
-        window.sessionStorage.setItem("Authorization",responseData.headers.get('Authorization')!);
-        this.model = <any> responseData.body;
+    this.loginService
+      .validateLoginDetails(this.model)
+      .subscribe((responseData) => {
+        //reading the JWT Token and storing it in local storage
+        window.sessionStorage.setItem(
+          'Authorization',
+          responseData.headers.get('Authorization')!
+        );
+        this.model = <any>responseData.body;
         this.model.authStatus = 'AUTH';
-        window.sessionStorage.setItem("userdetails",JSON.stringify(this.model));
+        window.sessionStorage.setItem(
+          'userdetails',
+          JSON.stringify(this.model)
+        );
         let xsrf = getCookie('XSRF-TOKEN')!;
-        window.sessionStorage.setItem("XSRF-TOKEN",xsrf);
+        window.sessionStorage.setItem('XSRF-TOKEN', xsrf);
         this.router.navigate(['dashboard']);
       });
-
   }
-
 }
