b3c0fc119a9f00772f737d0410e68177744b9868
maddrills <mathewfvv@gmail.com>
2024-04-29 15:33:48 +0530

    welcome page start

--- Spring_project 1/FrontEnd/loginSignupAuth/src/app/login-signup/login-signup.component.ts
+++ Spring_project 1/FrontEnd/loginSignupAuth/src/app/login-signup/login-signup.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { LoginComponent } from './login/login.component';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { SuccessComponent } from '../success/success.component';
 
 @Component({
   selector: 'app-login-signup',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, SuccessComponent],
   templateUrl: './login-signup.component.html',
   styleUrl: './login-signup.component.css',
 })
