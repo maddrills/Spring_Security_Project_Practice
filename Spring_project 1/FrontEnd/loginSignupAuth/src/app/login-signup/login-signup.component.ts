154eb0965d8ac5dee889511618ad9d69b6b2935b
maddrills <mathewfvv@gmail.com>
2024-04-29 18:39:02 +0530

    Admin Only Page added and styled

--- Spring_project 1/FrontEnd/loginSignupAuth/src/app/login-signup/login-signup.component.ts
+++ Spring_project 1/FrontEnd/loginSignupAuth/src/app/login-signup/login-signup.component.ts
@@ -3,11 +3,12 @@ import { LoginComponent } from './login/login.component';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { SuccessComponent } from '../success/success.component';
+import { AdminComponent } from '../admin/admin.component';
 
 @Component({
   selector: 'app-login-signup',
   standalone: true,
-  imports: [CommonModule, RouterModule, SuccessComponent],
+  imports: [CommonModule, RouterModule, SuccessComponent, AdminComponent],
   templateUrl: './login-signup.component.html',
   styleUrl: './login-signup.component.css',
 })
