95b71d699b86341f8ad446460e287d79dcdca749
maddrills <mathewfvv@gmail.com>
2024-05-03 17:00:40 +0530

    Log Out Button

--- Spring_project 1/FrontEnd/loginSignupAuth/src/app/login-signup/login-signup.component.ts
+++ Spring_project 1/FrontEnd/loginSignupAuth/src/app/login-signup/login-signup.component.ts
@@ -4,11 +4,12 @@ import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { SuccessComponent } from '../success/success.component';
 import { AdminComponent } from '../admin/admin.component';
+import { LogoutButtonComponent } from '../logout-button/logout-button.component';
 
 @Component({
   selector: 'app-login-signup',
   standalone: true,
-  imports: [CommonModule, RouterModule, SuccessComponent, AdminComponent],
+  imports: [CommonModule, RouterModule, SuccessComponent, AdminComponent,LogoutButtonComponent],
   templateUrl: './login-signup.component.html',
   styleUrl: './login-signup.component.css',
 })
---
+++ Spring_project 1/FrontEnd/loginSignupAuth/src/app/logout-button/logout-button.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-logout-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './logout-button.component.html',
+  styleUrl: './logout-button.component.css'
+})
+export class LogoutButtonComponent {
+
+}
